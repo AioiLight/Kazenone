681d1e3347389ef5364ed0bb459133cd9db6158e
AioiLight <info@aioilight.space>
2020-07-27 00:42:12 +0900

    テーマ設定

--- app/src/main/java/space/aioilight/kazenone/MainActivity.kt
+++ app/src/main/java/space/aioilight/kazenone/MainActivity.kt
@@ -2,10 +2,30 @@ package space.aioilight.kazenone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val theme = pref.getString("themes", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString())
+        AppCompatDelegate.setDefaultNightMode(when (theme) {
+            "0" -> {
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+            "1" -> {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+            "2" -> {
+                AppCompatDelegate.MODE_NIGHT_YES
+            }
+            else -> {
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+        })
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/space/aioilight/kazenone/SettingsFragment.kt
+++ app/src/main/java/space/aioilight/kazenone/SettingsFragment.kt
@@ -1,11 +1,35 @@
 package space.aioilight.kazenone
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        findPreference<ListPreference>("themes")?.apply {
+            setOnPreferenceChangeListener { preference, newValue ->
+                val id = newValue as String
+                AppCompatDelegate.setDefaultNightMode(when (id) {
+                    "0" -> {
+                        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    }
+                    "1" -> {
+                        AppCompatDelegate.MODE_NIGHT_NO
+                    }
+                    "2" -> {
+                        AppCompatDelegate.MODE_NIGHT_YES
+                    }
+                    else -> {
+                        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    }
+                })
+                true
+            }
+        }
     }
 }
